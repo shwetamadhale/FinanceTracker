907d7d893afd59dee19722c8c2a795d9744b40d0
shwetamadhale <madhaleshwetar@gmail.com>
2024-10-21 11:41:07 -0700

    Finished basic implementation

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,24 @@
+import express, { Express } from "express";
+import mongoose from "mongoose";
+import financialRecordRouter from "./routes/financial-records";
+import cors from "cors";
 
-import express, {Express} from 'express';
-import mongoose from 'mongoose';
-import financialRecordRouter from "./routes/financial-records"
 const app: Express = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(cors());
 
-const mongoURI: string = "mongodb+srv://madhaleshwetar:Lzu1Dz9BRKmdPKm6@personalfinancetracker.mh23n.mongodb.net/";
+const mongoURI: string =
+  "mongodb+srv://madhaleshwetar:uZQnwHNmHz6PEdjo@personalfinancetracker.qa21o.mongodb.net/";
 
 mongoose
-    .connect(mongoURI)
-    .then(() => console.log("CONNECTED TO MONGODB!"))
-    .catch((err) => console.error("Failed to Connect to MongoDB:", err));
-
+  .connect(mongoURI)
+  .then(() => console.log("CONNECTED TO MONGODB!"))
+  .catch((err) => console.error("Failed to Connect to MongoDB:", err));
 
 app.use("/financial-records", financialRecordRouter);
 
 app.listen(port, () => {
-    console.log('Server running on Port ${port}');
+  console.log(`Server Running on Port ${port}`);
 });
-
